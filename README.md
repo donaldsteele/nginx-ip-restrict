1b3edf68d99d1c1952d8a6c1403acb7db5373b72
Don <don.steele@gmal.com>
2018-08-15 00:07:49 -0400

    Added ability to block tor exit nodes. Started refactor for self refreshing ip lists.
    Signed-off-by: Don <don.steele@gmal.com>

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# nginx-ip-restrict
-rest service written in golang that acts as a nginx http basic auth end point that allows you to restrict connections based on country
+
+# nginx-ip-restrict  
+nginx-ip-restrict is a rest service written in golang /go that acts as a nginx http basic auth end point that allows you to restrict connections based on country with the option to also block tor end points. 
+
+
+
+### Usage 
+in this example we are restricting the all requests to wp-admin/*.php to only allow ip address from the United States
+
+    location ~* /wp-admin/.*\.php$ {
+	        auth_request     /auth;
+	        auth_request_set $auth_status $upstream_status;
+	        include snippets/fastcgi-php.conf;
+	        fastcgi_pass unix:/var/run/php/php7.0-fpm.sock;                
+	        fastcgi_cache_bypass $skip_cache;
+	        fastcgi_no_cache $skip_cache;
+	        fastcgi_cache WORDPRESS;
+	        fastcgi_cache_valid  60m;
+    }
+
+    location = /auth {
+            internal;
+            proxy_pass              http://127.0.0.1:9999/allowbycountry/us/$remote_addr;
+            proxy_pass_request_body off;
+            proxy_set_header        Content-Length "";
+            proxy_set_header        X-Original-URI $request_uri;
+        }
+
+
+the configuration file is simple json file and the structure is 
+
+    {  
+	  "listenAddress": "127.0.0.1",  
+	  "port": "9999",  
+	  "allowedCountries": [  
+		    "us",  
+			"ca"  
+	  ],  
+	  "blockTor": true,  
+	  "cacheDirectory": "./cache" 
+	}
+
+
+
+the current endpoint is  /allowbycountry/us/$ip_address
+if for example we use the ip 108.211.142.83 which is a known tor exit point 
+as verified here [as verified here](https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=8.8.8.8)  you can look up the country [here](https://www.infobyip.com/ipbulklookup.php)
+
+you will get a return of http status code 403 with the following json 
+
+	    {"code":403,"text":"UnAuthorized"}
+
+
+by contrast a known us address 5.150.156.21 that is not a tor exit node will return a http status 200 with the following json 
+
+    {"code":200,"text":"Authorized"}
--- main.go
+++ main.go
@@ -20,14 +20,62 @@ import (
 )
 
 var cidrCache = make(map[string]cidranger.Ranger)
+var torExitNodeCache []string
 var log = logrus.New()
+
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+type Routes []Route
+
+var routes = Routes{
+	Route{
+		"Index",
+		"GET",
+		"/",
+		Index,
+	},
+	Route{
+		"allowbycountry",
+		"GET",
+		"/allowbycountry/{country}/{ip}",
+		allowbycountry,
+	},
+}
+
+type jsonErr struct {
+	Code int    `json:"code"`
+	Text string `json:"text"`
+}
+
+type ConfigOptions struct {
+	listenAddress    string
+	port             string
+	allowedCountries []string
+	blockTor         bool
+	cacheDirectory   string
+}
+
+var Config = ConfigOptions{
+	"127.0.0.1",
+	"9999",
+	[]string{"us", "ca"},
+	true,
+	"./cache",
+}
 
 func main() {
 	initLog()
 	parseConfig()
 
-	cacheCIDR(viper.GetStringSlice("allowedCountries"))
-	var serverListenOn = viper.GetString("listenAddress") + ":" + viper.GetString("port")
+	cacheCIDR(Config.allowedCountries)
+	if Config.blockTor {
+		cacheTORExitNodes()
+	}
+	var serverListenOn = Config.listenAddress + ":" + Config.port
 	log.Info("Listening on " + serverListenOn)
 	log.Fatal(http.ListenAndServe(serverListenOn, NewRouter()))
 
@@ -55,6 +103,11 @@ func parseConfig() {
 	if err := viper.ReadInConfig(); err != nil {
 		log.Error("Error reading config file, %s", err)
 	}
+	Config.allowedCountries = viper.GetStringSlice("allowedCountries")
+	Config.listenAddress = viper.GetString("listenAddress")
+	Config.port = viper.GetString("port")
+	Config.cacheDirectory = viper.GetString("cacheDirectory")
+	Config.blockTor = viper.GetBool("blockTor")
 
 }
 
@@ -76,35 +129,6 @@ func NewRouter() *mux.Router {
 	return router
 }
 
-type Route struct {
-	Name        string
-	Method      string
-	Pattern     string
-	HandlerFunc http.HandlerFunc
-}
-
-type Routes []Route
-
-var routes = Routes{
-	Route{
-		"Index",
-		"GET",
-		"/",
-		Index,
-	},
-	Route{
-		"allowbycountry",
-		"GET",
-		"/allowbycountry/{country}/{ip}",
-		allowbycountry,
-	},
-}
-
-type jsonErr struct {
-	Code int    `json:"code"`
-	Text string `json:"text"`
-}
-
 func Index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Welcome!\n")
 }
@@ -114,18 +138,18 @@ func allowbycountry(w http.ResponseWriter, r *http.Request) {
 	var ip = vars["ip"]
 	var country = vars["country"]
 
-//	log.Info("country requested=" + country + " ip=" + ip)
+	//	log.Info("country requested=" + country + " ip=" + ip)
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	if checkIPinRange(country, ip) {
 
 		w.WriteHeader(http.StatusOK)
-		if err := json.NewEncoder(w).Encode(jsonErr{Code: http.StatusOK, Text: "Authorized for country " + country}); err != nil {
+		if err := json.NewEncoder(w).Encode(jsonErr{Code: http.StatusOK, Text: "Authorized"}); err != nil {
 			log.Error(err)
 		}
 
 	} else {
 		w.WriteHeader(http.StatusForbidden)
-		if err := json.NewEncoder(w).Encode(jsonErr{Code: http.StatusForbidden, Text: "UnAuthorized for country " + country}); err != nil {
+		if err := json.NewEncoder(w).Encode(jsonErr{Code: http.StatusForbidden, Text: "UnAuthorized"}); err != nil {
 			log.Error(err)
 		}
 	}
@@ -144,44 +168,74 @@ func httpLog(next http.Handler) http.Handler {
 }
 
 func cacheCIDR(countries []string) {
+	cidrCache = make(map[string]cidranger.Ranger)
 	for i := 0; i < len(countries); i++ {
 
 		log.Info("looking for country " + countries[i])
-		info, err := os.Stat(filepath.Join(viper.GetString("cacheDirectory"), countries[i]+"-aggregated.zone"))
+		info, err := os.Stat(filepath.Join(Config.cacheDirectory, countries[i]+"-aggregated.zone"))
 		if err == nil {
 			delta := time.Now().Sub(info.ModTime())
 
 			if delta.Hours() > 160 {
 				url := "http://www.ipdeny.com/ipblocks/data/aggregated/" + countries[i] + "-aggregated.zone"
-				downloadFromUrl(url)
+				downloadFromUrl(url, countries[i]+"-aggregated.zone")
 			} else {
 				log.Info(countries[i] + "-aggregated.zone is to new not downloading")
 			}
 		} else {
 			url := "http://www.ipdeny.com/ipblocks/data/aggregated/" + countries[i] + "-aggregated.zone"
-			downloadFromUrl(url)
+			downloadFromUrl(url, countries[i]+"-aggregated.zone")
 		}
 		readzonefile(countries[i])
 	}
 }
 
+func cacheTORExitNodes() {
+	log.Info("Inspecting tor exit node list")
+	info, err := os.Stat(filepath.Join(Config.cacheDirectory, "tor-exit-nodes.txt"))
+	if err == nil {
+		delta := time.Now().Sub(info.ModTime())
+
+		if delta.Hours() > 160 {
+			url := "https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=8.8.8.8"
+			downloadFromUrl(url, "tor-exit-nodes.txt")
+		} else {
+			log.Info("tor exit node list is to new not downloading")
+		}
+	} else {
+		url := "https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=8.8.8.8"
+		downloadFromUrl(url, "tor-exit-nodes.txt")
+	}
+	readTorExitNodes()
+}
+
 func checkIPinRange(country string, ip string) bool {
+
 	if rangerCountry, ok := cidrCache[country]; ok {
 		//do something here
-		contains, err := rangerCountry.Contains(net.ParseIP(ip)) // returns true, nil
+		inCountry, err := rangerCountry.Contains(net.ParseIP(ip)) // returns true, nil
+		if Config.blockTor {
+			isNotTorExitNode := stringInSlice(ip, torExitNodeCache) // returns true, nil
+			//flip the value since we want to know if they are NOT in the tor list
+			if err != nil {
+				log.Error(err)
+			}
+			isNotTorExitNode = !isNotTorExitNode
+			return inCountry && isNotTorExitNode
+		}
 		log.WithFields(logrus.Fields{
-			"ip": ip,
-			"country" : country,
-			"value" : strconv.FormatBool(contains),
-		}).Info( "")
+			"ip":      ip,
+			"country": country,
+			"value":   strconv.FormatBool(inCountry),
+		}).Info("")
 
 		if err != nil {
 			log.Error(err)
 		}
-		return contains
+		return inCountry
 	} else {
 		log.WithFields(logrus.Fields{
-			"country" : country,
+			"country": country,
 		})
 		log.Error("the country requested  is not in local zone cache, returning false")
 		return false
@@ -189,9 +243,50 @@ func checkIPinRange(country string, ip string) bool {
 	return false
 }
 
+func readTorExitNodes() {
+	var ipList []string
+	file, err := os.Open(filepath.Join(Config.cacheDirectory, "tor-exit-nodes.txt"))
+	if err != nil {
+		log.Error(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		newIP := strings.TrimSpace(scanner.Text())
+		if strings.HasPrefix(newIP, "#") {
+			continue
+		}
+
+		if len("newIP") < 2 {
+			continue
+		}
+		log.Info("adding " + scanner.Text())
+
+		if net.ParseIP(newIP) != nil {
+			ipList = append(ipList, newIP)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Error(err)
+	}
+
+	torExitNodeCache = ipList
+}
+
+func stringInSlice(str string, list []string) bool {
+	for _, v := range list {
+		if v == str {
+			return true
+		}
+	}
+	return false
+}
+
 func readzonefile(country string) {
 	var ranger = cidranger.NewPCTrieRanger()
-	file, err := os.Open(filepath.Join(viper.GetString("cacheDirectory"), country+"-aggregated.zone"))
+	file, err := os.Open(filepath.Join(Config.cacheDirectory, country+"-aggregated.zone"))
 	if err != nil {
 		log.Error(err)
 	}
@@ -211,15 +306,15 @@ func readzonefile(country string) {
 	cidrCache[country] = ranger
 }
 
-func downloadFromUrl(url string) {
-	tokens := strings.Split(url, "/")
-	fileName := tokens[len(tokens)-1]
+func downloadFromUrl(url string, fileName string) {
+	//	tokens := strings.Split(url, "/")
+	//	fileName := tokens[len(tokens)-1]
 
 	log.Info("Downloading", url, "to", fileName)
 
 	// TODO: check file existence first with io.IsExist
-	mkdirIFNotExists(viper.GetString("cacheDirectory"))
-	output, err := os.Create(filepath.Join(viper.GetString("cacheDirectory"), fileName))
+	mkdirIFNotExists(Config.cacheDirectory)
+	output, err := os.Create(filepath.Join(Config.cacheDirectory, fileName))
 	if err != nil {
 		log.Info("Error while creating", fileName, "-", err)
 		return
